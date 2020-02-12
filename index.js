5a7c4a1f1778a1a6b592435f83289cb8045fedc3
fbuitron <franciscobuitron21@gmail.com>
2020-02-12 15:16:15 -0800

    generates README based on user input

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-inquirer.prompt([{
+inquirer.prompt([
+{
   type: 'input',
   name: 'username',
   message: 'Please enter your GitHub username'
+},
+{
+  type: 'input',
+  name: 'title',
+  message: 'Enter your project title'
+},
+{
+  type: 'input',
+  name: 'desc',
+  message: 'Enter your project description'
+},
+{
+  type: 'input',
+  name: 'table',
+  message: 'Enter a table of contents'
+},
+{
+  type: 'input',
+  name: 'install',
+  message: 'Enter installation instructions'
+},
+{
+  type: 'input',
+  name: 'usage',
+  message: 'Enter the usage for your project'
+},
+{
+  type: 'input',
+  name: 'license',
+  message: 'Enter the project license'
+},
+{
+  type: 'input',
+  name: 'contributing',
+  message: 'Enter who contributed to the project'
+},
+{
+  type: 'input',
+  name: 'tests',
+  message: 'Enter tests for the project'
 }])
-.then(response => {
-  console.log(response)
+.then(({username, title, desc, table, install, usage, license, contributing, tests}) => {
+  generateMd(username, title, desc, table, install, usage, license, contributing, tests)
 })
+
+const generateMd = (username, title, desc, table, install, usage, license, contributing, tests) => {
+  fs.writeFile('README.md', `
+  # ${title}
+
+**Description**
+${desc}
+
+**Table of Contents**
+${table}
+
+**Installation**
+${install}
+
+**Usage**
+${usage}
+
+**License**
+${license}
+
+**Contributing**
+${contributing}
+
+**Tests**
+${tests}
+
+* ${username}
+* ${username}
+  `, error => error ? console.error(error) : console.log('success'))
+}
--- index.md
+++ index.md
@@ -1,27 +1,18 @@
-
-  # ahbag
+# Project Title
 
 **Description**
-sdfgjhd
 
 **Table of Contents**
-asgjhafg
 
 **Installation**
-afggf
 
 **Usage**
-afgadfg
 
 **License**
-aasg
 
 **Contributing**
-adfgafg
 
 **Tests**
-adfgfg
 
-* jxhcvbasf
-* jxhcvbasf
-  
+* User GitHub profile picture
+* User GitHub email
